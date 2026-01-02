d23a902f692db13049441170398649a22222f64a
Robert Lucas <rob576821@gmail.com>
2026-01-02 16:09:19 +0000

    Add argument to launch file

--- src/launch/pid_control.py
+++ src/launch/pid_control.py
@@ -1,10 +1,10 @@
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import SetEnvironmentVariable, IncludeLaunchDescription
+from launch.actions import SetEnvironmentVariable, IncludeLaunchDescription, DeclareLaunchArgument, LogInfo
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+# from launch_ros.substitutions import LaunchConfiguration
 import os
 
 def generate_launch_description():
@@ -13,7 +13,27 @@ def generate_launch_description():
     my_package_path = get_package_share_directory('self_balancing_robot')
     sdf_file = os.path.join(my_package_path, 'models', 'basic_sbr.sdf')
 
+    control_exec_config = LaunchConfiguration('control_exec')
+
+    control_exec_arg = DeclareLaunchArgument(
+        'control_exec',
+        default_value='robot_control_pid',
+        description='Parameter to control which control method is used.'
+    )
+    robot_control_node = Node(
+        package='self_balancing_robot',
+        executable=control_exec_config,
+        output='screen'
+    )
+
+    log_arg = LogInfo(
+        msg=['Launching robot with controller: ', control_exec_config]
+    )
+
     return LaunchDescription([
+        control_exec_arg,
+        log_arg,
+        robot_control_node,
         SetEnvironmentVariable(
             'GZ_SIM_RESOURCE_PATH',
             PathJoinSubstitution([my_package_path, 'models'])
@@ -38,11 +58,6 @@ def generate_launch_description():
                        '/imu@sensor_msgs/msg/Imu@gz.msgs.IMU',
                        '/world/empty/model/differential_drive_robot/joint_state@sensor_msgs/msg/JointState@gz.msgs.Model'
                        ],
-            output='screen'
-        ),
-        Node(
-            package='self_balancing_robot',
-            executable='robot_control_pid',
             output='screen'
         ),
     ])
